f5a002432a7cc6e3e724c43a369a0b701c52fdbf
PGluk <glukhov.p@gmail.com>
2021-07-29 09:50:25 +0500

    initial

--- backend/foodgram_project/settings.py
+++ backend/foodgram_project/settings.py
@@ -50,7 +50,7 @@ REST_FRAMEWORK = {
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.'
                                 'PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 6,
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.'
                                 'NamespaceVersioning',
 }
